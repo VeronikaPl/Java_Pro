911d9693c3480bde69206b22c9f91d606b93e77f
VeronikaPl <pletnovaroni@gmail.com>
2023-04-17 23:29:49 +0300

    Homework #18 done

---
+++ homework_18/Box.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Box<T extends Fruit> {
+    private final List<T> fruitList = new ArrayList<>();
+
+    public void add(T fruit) {
+        Objects.requireNonNull(fruit);
+        fruitList.add(fruit);
+    }
+
+    public void add(List<T> fruit) {
+        fruitList.addAll(fruit);
+    }
+
+    public double getWeight() {
+        if (fruitList.isEmpty()) {
+            return 0.0F;
+        }
+        double weight = 0;
+        for (T fruit : fruitList) {
+            weight += fruit.getWeight();
+        }
+        return weight;
+    }
+
+    public boolean compare(Box<?> box) {
+        Objects.requireNonNull(box);
+        return this.getWeight() == box.getWeight();
+    }
+
+    public void merge(Box<T> box) {
+        Objects.requireNonNull(box);
+        if (fruitList.get(0).getClass() != box.fruitList.get(0).getClass()) {
+            throw new IllegalArgumentException("Different fruit's types can't be merged");
+        } else {
+            fruitList.addAll(box.fruitList);
+            box.fruitList.clear();
+        }
+    }
+}
---
+++ homework_18/Fruit.java
@@ -0,0 +1,15 @@
+public class Fruit {
+    private double weight;
+
+    public Fruit(double weight) {
+        this.weight = weight;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+}
