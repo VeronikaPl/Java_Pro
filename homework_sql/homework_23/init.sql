b7bc8ccc02cef9569bc253845eb7dfb465b96bab
VeronikaPl <pletnovaroni@gmail.com>
2023-05-08 14:44:21 +0300

    Homework #24 done

--- homework_sql/homework_23/init.sql
+++ homework_sql/homework_23/init.sql
@@ -23,3 +23,21 @@ lesson_id BIGINT REFERENCES lesson(id) ON DELETE CASCADE,
 schedule_id BIGINT REFERENCES schedule(id) ON DELETE CASCADE,
 PRIMARY KEY (lesson_id, schedule_id)
 );
+
+INSERT FROM homework (name, description) VALUES
+('Homework #1', 'Do exercise 10-13'),
+('Homework #2', 'Take a short test to revise topic');
+
+INSERT INTO lesson (name, homework_id) VALUES
+('Lesson #1', 1),
+('Lesson #2', 1),
+('Lesson #3', 2);
+
+INSERT INTO schedule (name) VALUES
+('Schedule #1'),
+('Schedule #2');
+
+INSERT INTO lesson_schedule (lesson_id, schedule_id) VALUES
+(1, 1),
+(2, 1),
+(3, 2);
